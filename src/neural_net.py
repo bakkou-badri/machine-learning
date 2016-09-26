18c215c5846e190871eb0c4f4e791b7e2c24d244
Vojtech Cima <cima.vojtech@gmail.com>
2016-09-26 20:50:54 +0200

    [src] neural_net: use class variables

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+
 class NeuralNet:
     """ NeuralNet class
     An implementation of a multilayered artificial neural network
@@ -9,27 +10,31 @@ class NeuralNet:
     of the last layer must match the number of classes (labels).
     """
 
-    def __init__(self, learning_rate, network_shape, session):
+    def __init__(self, learning_rate, shape, session):
         self.session = session
         self.learning_rate = learning_rate
-        self.network_shape = network_shape
-        self.weights = self.init_weights(network_shape)
-        self.biases = self.init_biases(network_shape)
+        self.shape = shape
+        self.weights = self.init_weights()
+        self.biases = self.init_biases()
 
         # Placeholders for inputs and labels
-        self.x = tf.placeholder(tf.float32, [None, network_shape[0]])
-        self.y_ = tf.placeholder(tf.float32, [None, network_shape[-1]])
+        self.x = tf.placeholder(tf.float32, [None, shape[0]])
+        self.y_ = tf.placeholder(tf.float32, [None, shape[-1]])
 
         # Neural network model
-        self.y = self.feed_forward(self.x, self.weights, self.biases)
+        self.y = self.feed_forward()
 
         # Define loss J and optimizer
-        self.J = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.y, self.y_))
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.J)
+        self.J = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(self.y, self.y_))
+        self.optimizer = tf.train.AdamOptimizer(
+            learning_rate=learning_rate).minimize(self.J)
 
         # Define accuracy calculation
-        self.correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        self.correct_prediction = tf.equal(
+            tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
+        self.accuracy = tf.reduce_mean(
+            tf.cast(self.correct_prediction, tf.float32))
         tf.initialize_all_variables().run(session=self.session)
 
     def train(self, max_iter, data_set):
@@ -44,56 +49,57 @@ class NeuralNet:
                 print("Iteration: %s, Accuracy (ts): %s, Accuracy (vs): %s" %
                       (i, self.accuracy.eval({self.x: batch[0],
                                               self.y_: batch[1]},
-                                              session=self.session),
+                                             session=self.session),
                        self.accuracy.eval({self.x: data_set.test.images,
                                            self.y_: data_set.test.labels},
-                                           session=self.session)))
+                                          session=self.session)))
 
-    def init_weights(self, shape):
+    def init_weights(self):
         """ Randomly initializes weight matrices.
-        :param shape: network shape tuple
         :return: weight matrices
         """
         weights = {}
-        for i in range(len(shape)-1):
-            weights[i] = tf.Variable(tf.random_normal([shape[i], shape[i+1]]))
+        for i in range(len(self.shape) - 1):
+            weights[i] = tf.Variable(tf.random_normal(
+                [self.shape[i], self.shape[i + 1]]))
         return weights
 
-    def init_biases(self, shape):
+    def init_biases(self):
         """ Randomly initializes biases vectors.
-        :param shape: network shape tuple
         :return: bias vectors
         """
         biases = {}
-        for i in range(len(shape)-1):
-            biases[i] = tf.Variable(tf.random_normal([shape[i+1]]))
+        for i in range(len(self.shape) - 1):
+            biases[i] = tf.Variable(tf.random_normal([self.shape[i + 1]]))
         return biases
 
-    def feed_forward(self, x, weights, biases):
+    def feed_forward(self):
         """ Creates the neural network representation and workflow.
-        :param x: input matrix
-        :param weights: weight matrices
-        :param biases: bias vectors
         :return: UNSCALED output of the last layer
         """
         l = None
-        for i in range(len(weights)):
+        layer_idx = len(self.shape) - 1
+        for i in range(layer_idx):
             if l is None:
                 # Use X as an input for the first layer.
-                l = tf.nn.relu(tf.add(tf.matmul(x, weights[i]), biases[i]))
-            elif i < len(weights) - 1:
+                l = tf.nn.relu(
+                    tf.add(tf.matmul(self.x, self.weights[i]), self.biases[i]))
+            elif i < layer_idx - 1:
                 # Use the output of the previous layer as the inputs for all
                 # hidden layers.
-                l = tf.nn.relu(tf.add(tf.matmul(l, weights[i]), biases[i]))
+                l = tf.nn.relu(
+                    tf.add(tf.matmul(l, self.weights[i]), self.biases[i]))
             else:
                 # Don't scale the output of the last layer as it's taken
                 # care of within the softmax_cross_entropy_with_logits
                 # function when calculating the error.
-                l = (tf.add(tf.matmul(l, weights[i]), biases[i]))
+                l = (tf.add(tf.matmul(l, self.weights[i]), self.biases[i]))
         return l
+
 
 def main():
     learning_rate = 0.01
+    max_iter = 10000
 
     # Define network shape as follows:
     #  * input layer matches the data dimension (784 pixels per image).
@@ -102,13 +108,13 @@ def main():
     # Note: while the dimension of the first and the last layer must match
     # dataset parameters, the number of hidden layers and the number of neurons
     # in these layers can be customised and will affect the classification
-    network_shape = (784, 256, 256, 10)
-    
+    shape = (784, 256, 256, 10)
+
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     session = tf.Session()
-    nn = NeuralNet(learning_rate, network_shape, session)
-    nn.train(20000, mnist)
+    nn = NeuralNet(learning_rate, shape, session)
+    nn.train(max_iter, mnist)
     session.close()
 
 if __name__ == "__main__":
