e3bea84a976b5d7cba45f35bb0d75b80cc7be3af
Vojtech Cima <cima.vojtech@gmail.com>
2016-09-26 16:09:11 +0200

    [src] Use class variables; allow explicit specification of layer index for feed forward process (enables to check output values y on each layer if needed.)

--- src/autoencoder.py
+++ src/autoencoder.py
@@ -5,25 +5,25 @@ class Autoencoder:
     """ Autoencoder class
     Symetric neural network reconstructing X' from X.
     """
-    def __init__(self, learning_rate, network_shape, session):
-        if len(network_shape) != 3 or network_shape[0] != network_shape[-1]:
-            print "Invalid shape %s" % (network_shape)
+    def __init__(self, learning_rate, shape, session):
+        if len(shape) != 3 or shape[0] != shape[-1]:
+            print "Invalid shape %s" % (shape)
             print "Autoencoder must be a symetric 3-layered network"
             print "Exiting!"
             exit(-1)
         self.session = session
         self.display_step = 100
         self.learning_rate = learning_rate
-        self.network_shape = network_shape
-        self.weights = self.init_weights(network_shape)
-        self.biases = self.init_biases(network_shape)
+        self.shape = shape
+        self.weights = self.init_weights()
+        self.biases = self.init_biases()
 
         # Placeholders for inputs and labels
-        self.x = tf.placeholder(tf.float32, [None, network_shape[0]])
-        self.y_ = tf.placeholder(tf.float32, [None, network_shape[-1]])
+        self.x = tf.placeholder(tf.float32, [None, shape[0]])
+        self.y_ = tf.placeholder(tf.float32, [None, shape[-1]])
 
         # Autoencoder model
-        self.y = self.feed_forward(self.x, self.weights, self.biases)
+        self.y = self.feed_forward()
 
         # Define loss J and optimizer
         self.J = tf.reduce_mean(tf.pow(self.y - self.y_, 2))
@@ -41,38 +41,49 @@ class Autoencoder:
                 print "Iteration: %s, J: %s" % (i, self.J.eval({self.x: batch[0],
                                                                 self.y_: batch[0]},
                                                 session=self.session))
-
-    def init_weights(self, shape):
+    def init_weights(self):
         weights = {}
-        for i in range(len(shape)-1):
-            weights[i] = tf.Variable(tf.random_normal([shape[i], shape[i+1]]))
+        for i in range(len(self.shape)-1):
+            weights[i] = tf.Variable(tf.random_normal([self.shape[i], self.shape[i+1]]))
         return weights
 
-    def init_biases(self, shape):
+    def init_biases(self):
         biases = {}
-        for i in range(len(shape)-1):
-            biases[i] = tf.Variable(tf.random_normal([shape[i+1]]))
+        for i in range(len(self.shape)-1):
+            biases[i] = tf.Variable(tf.random_normal([self.shape[i+1]]))
         return biases
 
-    def feed_forward(self, x, weights, biases):
+    def feed_forward(self, layer_idx=-1):
+        """ Feeds the network forward up to layer_idx.
+        The network is fed all the way through if the layer_idx
+        parameter is not specified. If the layer_idx is specified,
+        the network is fed UP TO layer_idx value. Therefore the
+        layer_idx value must be within the <1, len(self.shape) -1>
+        interval.
+        :param layer_idx: layer index
+        :return: output of layer[layer_idx]
+        """
         l = None
-        for i in range(len(weights)):
+        if layer_idx < 0:
+            # If layer_idx not specified explicitly, feed all layers
+            layer_idx = len(self.shape) - 1
+        for i in range(layer_idx):
             if l is None:
-                l = tf.nn.sigmoid(tf.add(tf.matmul(x, weights[i]), biases[i]))
+                l = tf.nn.sigmoid(tf.add(tf.matmul(self.x, self.weights[i]), self.biases[i]))
             else:
-                l = tf.nn.sigmoid(tf.add(tf.matmul(l, weights[i]), biases[i]))
+                l = tf.nn.sigmoid(tf.add(tf.matmul(l, self.weights[i]), self.biases[i]))
         return l
 
 def main():
     learning_rate = 0.01
     max_iter = 10000
 
-    network_shape = (784, 128, 784)
+    shape = (784, 128, 784)
 
     mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
     session = tf.Session()
-    ae = Autoencoder(learning_rate, network_shape, session)
+    ae = Autoencoder(learning_rate, shape, session)
     ae.train(max_iter, mnist)
     session.close()
 
